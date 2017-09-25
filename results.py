f6c40e11430ac010e663767de5c9deaa6aa0b00c
AaronRito <aaronrito@gmail.com>
2017-09-25 15:53:37 -0700

    stable

--- results.py
+++ results.py
@@ -25,44 +25,41 @@ class BoxResults(QtCore.QObject):
         self.acceptTime_path = None
         self.shockModeTime_path = None
         self.shockedTime_path = None
+        self.res = []
 
     def results_init(self, box_id):
 
-        self.acceptSide = [self.settings.value("control_number/box_id_" + str(box_id)),
-                           self.settings.value("concentrate/box_id_" + str(box_id)),  ("Shuttlebox_" + str(box_id) +
-                                                                                       "_on_" +
-                                                                                       QtCore.QDateTime.currentDateTime(
-                                                                                       ).toString(QtCore.Qt.ISODate))]
-        self.seekSideSwaps = [self.settings.value("control_number/box_id_" + str(box_id)),
-                              self.settings.value("concentrate/box_id_" + str(box_id)), ("Shuttlebox_" + str(box_id) +
-                                                                                         "_on_" + QtCore.QDateTime.
-                                                                                         currentDateTime().toString
-                                                                                         (QtCore.Qt.ISODate))]
-        self.numSideSwaps = [self.settings.value("control_number/box_id_" + str(box_id)),
-                              self.settings.value("concentrate/box_id_" + str(box_id)), (
-                              "Shuttlebox_" + str(box_id) + "_on_" + QtCore.QDateTime.currentDateTime().toString(
-                                  QtCore.Qt.ISODate))]
-        self.timeToAccept = [self.settings.value("control_number/box_id_" + str(box_id)), self.settings.value
-        ("concentrate/box_id_" + str(box_id)), ("Shuttlebox_" + str(box_id) + "_on_" + QtCore.QDateTime.currentDateTime(
-        ).toString(QtCore.Qt.ISODate))]
-        self.rejectTime = [self.settings.value("control_number/box_id_" + str(box_id)),
-                           self.settings.value("concentrate/box_id_" + str(box_id)), ("Shuttlebox_" + "_on_" +
-                           QtCore.QDateTime.currentDateTime().toString(QtCore.Qt.ISODate))]
-        self.acceptTime = [self.settings.value("control_number/box_id_" + str(box_id)), self.settings.value
-        ("concentrate/box_id_" + str(box_id)), ("Shuttlebox_" + str(box_id) + "_on_" +
-                                                QtCore.QDateTime.currentDateTime().toString(QtCore.Qt.ISODate))]
-        self.shockModeTime = [self.settings.value("control_number/box_id_" + str(box_id)),
-                              self.settings.value("concentrate/box_id_" + str(box_id)), (
-                              "Shuttlebox_" + str(box_id) + "_on_" + QtCore.QDateTime.currentDateTime().toString(
-                                  QtCore.Qt.ISODate))]
-        self.shockedTime = [self.settings.value("control_number/box_id_" + str(box_id)), self.settings.value
-        ("concentrate/box_id_" + str(box_id)), ("Shuttlebox_" + str(box_id) + "_on_" +
-                                                QtCore.QDateTime.currentDateTime().toString(QtCore.Qt.ISODate))]
+        #initialize the arrays, used for clearing old data as well
+        self.acceptSide = self.fill_arrays(box_id)
+        self.seekSideSwaps = self.fill_arrays(box_id)
+        self.numSideSwaps = self.fill_arrays(box_id)
+        self.timeToAccept = self.fill_arrays(box_id)
+        self.rejectTime = self.fill_arrays(box_id)
+        self.acceptTime = self.fill_arrays(box_id)
+        self.shockModeTime = self.fill_arrays(box_id)
+        self.shockedTime = self.fill_arrays(box_id)
 
-        self.acceptSide_path = ("C:/Users/AaronR/PycharmProjects/shuttle_gui/box_results/" + self.settings.value(
-            "control_number/box_id_" + str(box_id)) + "/acceptSide")
-        if not os.path.isdir(self.acceptSide_path):
-            os.makedirs(self.acceptSide_path)
+        #make the file paths if the do not exist already
+        self.make_folders(box_id, "/acceptSide")
+        self.make_folders(box_id, "/seekSideSwaps")
+        self.make_folders(box_id, "/numSideSwaps")
+        self.make_folders(box_id, "/timeToAccept")
+        self.make_folders(box_id, "/rejectTime")
+        self.make_folders(box_id, "/acceptTime")
+        self.make_folders(box_id, "/shockModeTime")
+        self.make_folders(box_id, "/shockedTime")
+
+    def fill_arrays(self, box_id):
+        temp = [self.settings.value("control_number/box_id_" + str(box_id)), self.settings.value("concentrate/box_id_" +
+                                                                                                 str(box_id)), (
+            "Shuttlebox_" + str(box_id) + "_on_" + QtCore.QDateTime.currentDateTime().toString(QtCore.Qt.ISODate))]
+        return temp
+
+    def make_folders(self, box_id, file_ending):
+        path = ("C:/Users/AaronR/PycharmProjects/shuttle_gui/box_results/" + self.settings.value(
+            "control_number/box_id_" + str(box_id)) + file_ending)
+        if not os.path.isdir(path):
+            os.makedirs(path)
 
     def results_to_array(self, results_array, box_id):
         self.acceptSide.append(results_array[0])
@@ -70,23 +67,31 @@ class BoxResults(QtCore.QObject):
         m = self.settings.value(("boxes/box_id_" + str(box_id) + "/n_of_trials"))
         print("n of trials = ", m)
         if int(m) == int(results_array[0]):
-            self.acceptSide[0] = self.settings.value("control_number/box_id_" + str(box_id))
-            self.acceptSide[1] = self.settings.value("concentrate/box_id_" + str(box_id))
-            self.acceptSide[2] = ("Shuttlebox_" + str(box_id) + "_on_" +
-                                  QtCore.QDateTime.currentDateTime().toString(QtCore.Qt.ISODate))
-            print("writing string to file: ", self.acceptSide)
-            file = open("C:/Users/AaronR/PycharmProjects/shuttle_gui/box_results/" + self.settings.value(
-                "control_number/box_id_" + str(box_id)) + "/acceptSide/acceptSide.txt", "a")
-            for i in range(0, (3 + int(m))):
-                if i == (2 + int(m)):
-                    file.write(str(self.acceptSide[i]))
-                    file.write("\n")
-                else:
-                    file.write(str(self.acceptSide[i]))
-                    file.write(",")
-            file.close()
-            self.acceptSide = [self.settings.value("control_number/box_id_" + str(box_id)),
-                               self.settings.value("concentrate/box_id_" + str(box_id)), ("Shuttlebox_" + str(box_id) +
-                                                                                          "_on_" + QtCore.QDateTime.
-                                                                                          currentDateTime().toString(
-                                                                                            QtCore.Qt.ISODate))]
+            self.save_results(box_id, self.acceptSide, "/acceptSide/acceptSide.txt")
+            self.acceptSide = self.update_results_array()
+            print(self.acceptSide)
+
+    def save_results(self, box_id, results_array, file_ending):
+        self.res = results_array
+        self.res[0] = self.settings.value("control_number/box_id_" + str(box_id))
+        self.res[1] = self.settings.value("concentrate/box_id_" + str(box_id))
+        self.res[2] = ("Shuttlebox_" + str(box_id) + "_on_" +
+                              QtCore.QDateTime.currentDateTime().toString(QtCore.Qt.ISODate))
+        print("writing string to file: ", self.res)
+        file = open("C:/Users/AaronR/PycharmProjects/shuttle_gui/box_results/" + self.settings.value(
+            "control_number/box_id_" + str(box_id)) + file_ending, "a")
+        m = self.settings.value(("boxes/box_id_" + str(box_id) + "/n_of_trials"))
+        for i in range(0, (3 + int(m))):
+            if i == (2 + int(m)):
+                file.write(str(self.res[i]))
+                file.write("\n")
+            else:
+                file.write(str(self.res[i]))
+                file.write(",")
+        file.close()
+        self.res = [self.settings.value("control_number/box_id_" + str(box_id)), self.settings.value(
+            "concentrate/box_id_" + str(box_id)), ("Shuttlebox_" + str(box_id) + "_on_" + QtCore.QDateTime.
+                                                   currentDateTime().toString(QtCore.Qt.ISODate))]
+
+    def update_results_array(self):
+        return self.res
